16e359c74b6a10c3b03b33206132eba066ae34ae
Udit Karode <udit.karode@gmail.com>
2021-06-11 13:26:22 +0530

    [feat:database] generate schema and connect to db

---
+++ src/database/schema.rs
@@ -0,0 +1,36 @@
+// generated using `diesel migration run`
+
+table! {
+    config (c_id) {
+        c_id -> Integer,
+        c_name -> Text,
+        c_content -> Text,
+    }
+}
+
+table! {
+    package (p_id) {
+        p_id -> Integer,
+        p_token -> Text,
+        p_name -> Text,
+        p_version -> Text,
+    }
+}
+
+table! {
+    user (id) {
+        id -> Integer,
+        username -> Text,
+        email -> Text,
+        password -> Text,
+        token -> Text,
+        verification_code -> Nullable<Text>,
+        verified -> Bool,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    config,
+    package,
+    user,
+);
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,48 @@
 mod errors;
 mod routes;
+mod database;
 
 use actix_web::{web, App, HttpServer};
+use diesel::prelude::{SqliteConnection, Connection};
 use handlebars::Handlebars;
+use dotenv::dotenv;
+use std::env;
 
-#[macro_use]
-extern crate serde_json;
+#[macro_use] extern crate serde_json;
+#[macro_use] extern crate diesel;
 
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+fn setup_handlebars() -> actix_web::web::Data<handlebars::Handlebars<'static>> {
     let mut handlebars = Handlebars::new();
 
     handlebars
         .register_templates_directory(".html", "./static/templates")
         .unwrap();
 
-    let handlebars_ref = web::Data::new(handlebars);
+    web::Data::new(handlebars)
+}
 
-    let port = 8080;
-    let host = "127.0.0.1";
+fn establish_connection() -> SqliteConnection {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    SqliteConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let host = env::var("REGISTRY_HOST").expect("REGISTRY_HOST must be set");
+    let port = env::var("REGISTRY_PORT").expect("REGISTRY_PORT must be set");
+
+    let connection = establish_connection();
 
     println!("Starting server on {}:{}", host, port);
 
     HttpServer::new(move || {
         App::new()
             .wrap(errors::handlers())
-            .app_data(handlebars_ref.clone())
+            .app_data(setup_handlebars())
             .service(routes::home::index)
     }).bind(("127.0.0.1", 8080))?
       .run()
