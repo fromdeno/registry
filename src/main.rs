425b340bceb4dac66c539ed2b38054350e7761ab
Udit Karode <udit.karode@gmail.com>
2021-06-12 19:15:04 +0530

    [fix:main] convert `format!`ed String to str

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn establish_connection() -> SqliteConnection {
 	let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
 	SqliteConnection::establish(&database_url)
-		.expect(format!("Error connecting to {}", database_url))
+		.expect(format!("Error connecting to {}", database_url).as_str())
 }
 
 #[actix_web::main]
